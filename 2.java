b2efeac3aca3a162ca1d48de35d4aeef8bcd6631
Anshal Nagvanshi <anshalnagvanshi1177@gmail.com>
2025-07-31 11:53:14 +0530

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int age = sc.nextInt();
+        System.out.println("My age is " + age); 
+    }
+}
