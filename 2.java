fcc2443415188ce3b23d21ebef3e8dbe8fa6d188
Anshal Nagvanshi <anshalnagvanshi1177@gmail.com>
2025-08-18 11:18:44 +0530

    Update and rename 2.java to Age.java

--- 2.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        int age = sc.nextInt();
-        System.out.println("My age is " + age); 
-    }
-}
